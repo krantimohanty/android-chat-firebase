db6bc62b5c67391f1c92b9f2f59d179c4b1db1cc
Kranti Bijay Mohanty <kranti@kencloud.co.in>
2017-02-17 15:56:15 +0530

    Initial commit

---
+++ app/src/main/java/com/kenchatdemo/UserDetails.java
@@ -0,0 +1,13 @@
+package com.kenchatdemo;
+
+/**
+ * Created by shukra on 17/2/2016.
+ *
+ * Initialize Firebase with the application context. This must happen before the client is used.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
